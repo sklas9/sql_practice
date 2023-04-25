d0222a7c24dc31d82760345cb6032742456456af
Srilakshmi S <sri.iuac@gmail.com>
2023-04-25 10:01:18 +0530

    Update friends_score.sql

--- friends_score.sql
+++ friends_score.sql
@@ -5,14 +5,19 @@ create table person(
     Score int 
     );
 
+create table friend(
+    PersonID varchar(10),
+    FriendID varchar(10)
+    );
+
 # Q) Find personID, name, number of friends, sum of marks of person who has friends with total score greater than 100
 # source - https://www.youtube.com/watch?v=SfzbR69LquU&list=PLBTZqjSKn0IeKBQDjLmzisazhqQy4iGkb&index=6
 
 with friends_score as 
 (
-    select distinct f.PersonID,f.FriendID, p.Score
-    from person p join friend f
-    on p.PersonID = f.FriendID
+    select distinct f.PersonID, f.FriendID, p.Score 
+    from friend f join person p
+    on f.FriendID = p.PersonID;
 )
 
 select p.PersonID, p.Name, count(distinct fs.FriendID), sum(p.score) as my_score, sum(fs.score) as friends_score
