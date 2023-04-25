58b8c16d9a49bf495effa665f60692ba07cda26c
Srilakshmi S <sri.iuac@gmail.com>
2023-04-25 10:36:30 +0530

    Update new_returning_cust.sql
    added create table command

--- new_returning_cust.sql
+++ new_returning_cust.sql
@@ -1,6 +1,19 @@
-#Find the new or returning customers count on each day
+/*
+create table customer_orders (
+order_id integer,
+customer_id integer,
+order_date date,
+order_amount integer
+);
 
-#optimized version
+insert into customer_orders values(1,100,cast('2022-01-01' as date),2000),(2,200,cast('2022-01-01' as date),2500),(3,300,cast('2022-01-01' as date),2100)
+,(4,100,cast('2022-01-02' as date),2000),(5,400,cast('2022-01-02' as date),2200),(6,500,cast('2022-01-02' as date),2700)
+,(7,100,cast('2022-01-03' as date),3000),(8,400,cast('2022-01-03' as date),1000),(9,600,cast('2022-01-03' as date),3000);
+*/
+
+/*Find the new or returning customers count on each day*/
+
+/*optimized version*/
 with cust_first_purchase as (
 	    select customer_id, min(order_date) as f_p_date
 	    from customer_orders
