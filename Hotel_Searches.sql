87d0ada06f80613bf8d206e7fff69df2fe2f5470
Srilakshmi S <sri.iuac@gmail.com>
2023-04-26 22:02:42 +0530

    Create Hotel_Searches.sql

---
+++ Hotel_Searches.sql
@@ -0,0 +1,15 @@
+/*Find the total number of searches for each room type (apartments, private, shared) .*/
+
+create table Hotel_Searches 
+(
+User_ID int,
+Search_Date date,
+Room_Type_Filter varchar(200)
+);
+insert into Hotel_Searches values
+(1,'2022-01-01','entire home,private room, shared room')
+,(2,'2022-01-02','entire home,shared room')
+,(3,'2022-01-02','private room,shared room')
+,(4,'2022-01-03','private room')
+;
+
