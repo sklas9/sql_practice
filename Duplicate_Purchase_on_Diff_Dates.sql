c400f2a0f009d7ddba08eb4ed5e4423b3e0dc0bd
Srilakshmi S <sri.iuac@gmail.com>
2023-05-01 12:54:10 +0530

    Update Duplicate_Purchase_on_Diff_Dates.sql

--- Duplicate_Purchase_on_Diff_Dates.sql
+++ Duplicate_Purchase_on_Diff_Dates.sql
@@ -1,6 +1,13 @@
+/* Source - https://datalemur.com/questions/sql-repeat-purchases */
+
 create table Purchases(
     Cust_ID int,
     Product_ID int,
     Quantity int,
     Purchase_Date datetime
     );
+
+select Cust_ID, Product_ID, count(distinct Purchase_Date) as cnt_same_prod_diff_day
+from Purchases
+group by Cust_ID, Product_ID
+having cnt_same_prod_diff_day > 1
