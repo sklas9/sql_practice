f74ab533c56bd753db3e4607a77520de13a0bd00
Srilakshmi S <sri.iuac@gmail.com>
2023-05-06 21:32:09 +0530

    Update Percentage_Change.sql

--- Percentage_Change.sql
+++ Percentage_Change.sql
@@ -3,6 +3,19 @@ create table Sales(
     Date date,
     Amount float
     )
+    
+    
+with combined as(
+    select ID, lead(Amount,1,0) over(order by Date asc) as New_Amount
+	from Sales
+    )
+    
+select s.*, New_Amount, round((New_Amount - Amount)/Amount * 100,2) as Percentage_Change
+from Sales s join combined c on s.ID = c.ID
+order by date;
+
+/* Alternative */
+
 
 with ranked as(
     select *, row_number() over(order by date) as row_num
@@ -11,4 +24,4 @@ with ranked as(
     
 select new.Date, new.Amount as New_Amount, old.Amount as Original_Amount,
 round((new.Amount - old.Amount)/old.Amount * 100) as Percentage_Change
-from ranked new left join ranked old on new.row_num = old.row_num + 1
+from ranked new left join ranked old on new.row_num = old.row_num + 1;
